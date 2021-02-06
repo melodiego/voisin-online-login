0d927c24d0dd9512307c29584bbc057e619bc9c4
Diego Melo <diego.melo@dbccompany.com.br>
2021-02-06 12:19:42 -0300

    Structure inital of project

---
+++ src/main/java/br/com/voisinonline/login/VoisinOnlineLoginApplication.java
@@ -0,0 +1,13 @@
+package br.com.voisinonline.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoisinOnlineLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoisinOnlineLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/voisinonline/login/VoisinOnlineLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.voisinonline.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoisinOnlineLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
